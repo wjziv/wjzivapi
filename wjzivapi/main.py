807bc6beda7e6eeacee032e9fad624dbbe3eee6c
Walter Zielenski <walterzielenski@gmail.com>
2021-01-09 13:51:25 -0800

    demo

--- wjzivapi/main.py
+++ wjzivapi/main.py
@@ -1,9 +1,64 @@
-from fastapi import FastAPI
+from typing import List
 
-__doc__ = """An API by WJZIV"""
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# SQLAlchemy specific code, as with any other app (public)
+DATABASE_URL = "postgresql://doadmin:izx23kfmj5a9b913@db-postgresql-nyc1-88661-do-user-8514409-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return { "message": "This is an API!" }
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
