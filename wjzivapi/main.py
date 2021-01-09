7e8eb6ce3bc6567bac985f051c4d0c728fe8f9a0
Walter Zielenski <walterzielenski@gmail.com>
2021-01-09 15:50:54 -0800

    remove check same thread arg

--- wjzivapi/main.py
+++ wjzivapi/main.py
@@ -23,7 +23,6 @@ notes = sqlalchemy.Table(
 
 engine = sqlalchemy.create_engine(
     DATABASE_URL, # connect_args={"check_same_thread": False}
-    check_same_thread=False,
 )
 metadata.create_all(engine)
 
