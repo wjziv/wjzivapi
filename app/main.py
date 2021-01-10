54a70a974bb35f2b0aa73774126e4ae3649767fe
Walter Zielenski <walterzielenski@gmail.com>
2021-01-09 19:27:37 -0800

    add echo

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ notes = sqlalchemy.Table(
 # https://www.postgresql.org/docs/9.1/libpq-connect.html
 engine = sqlalchemy.create_engine(
     DATABASE_URL,
+    echo=True,
     connect_args={
         'sslmode': 'require',
         'sslrootcert': '/app/certs/postgres.crt'
